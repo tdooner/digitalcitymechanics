ea5a3bad7f8a33af8b58e4185a50856dedf5e455
Tom Dooner <tomdooner@gmail.com>
2010-07-26 20:59:24 -0400

    Gets a wordpress in good shape for the DCM layout.

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'digitalcitymechanics');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'digitalcity');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'digitalcity');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'HEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
+define('SECURE_AUTH_KEY',  'GEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
+define('LOGGED_IN_KEY',    'IEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
+define('NONCE_KEY',        'JEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
+define('AUTH_SALT',        'KEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
+define('SECURE_AUTH_SALT', 'LEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
+define('LOGGED_IN_SALT',   'MEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
+define('NONCE_SALT',       'NEC()(#0euthjututututara9,RC"GPGPGPGPGPGGPPr.c,ego0202020239***#()@tehub,n');
 
 /**#@-*/
 
